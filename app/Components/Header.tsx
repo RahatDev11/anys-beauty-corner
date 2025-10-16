916a7d59891d86654f2c88beefe58ec5e2fb6682
RahatDev11 <148705683+RahatDev11@users.noreply.github.com>
2025-10-17 01:54:41 +0600

    Update Header.tsx

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,4 +1,3 @@
-
 // app/components/Header.tsx
 'use client';
 
@@ -191,4 +190,4 @@ export default function Header() {
       <div id="toast" className="fixed bottom-4 right-4 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg hidden z-50"></div>
     </>
   );
-      }
+}
