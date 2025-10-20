646f9f3e96d28e1c4945e6488560a7de60c2c389
RahatDev11 <148705683+RahatDev11@users.noreply.github.com>
2025-10-21 00:30:28 +0600

    Create providers.js

---
+++ app/providers.js
@@ -0,0 +1,7 @@
+// app/providers.js
+'use client';
+import { SessionProvider } from "next-auth/react";
+
+export function AuthProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
