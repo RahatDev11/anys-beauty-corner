870448cb6122a58efabc96df1e1f240e2818ed88
RahatDev11 <148705683+RahatDev11@users.noreply.github.com>
2025-10-21 01:17:43 +0600

    Update providers.js

--- app/providers.js
+++ app/providers.js
@@ -1,7 +1,7 @@
-// app/providers.tsx
+// app/providers.js
 'use client';
 import { SessionProvider } from "next-auth/react";
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
+export function AuthProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
