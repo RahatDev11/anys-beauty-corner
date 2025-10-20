823d38dd2a0c8d8ec2e3b4a42ee086d23f27bc25
RahatDev11 <148705683+RahatDev11@users.noreply.github.com>
2025-10-21 01:16:33 +0600

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "../../../../auth.config";
 
-export const { handlers: { GET, POST }, auth } = NextAuth(authConfig);
+const handler = NextAuth(authConfig);
+
+export { handler as GET, handler as POST };
